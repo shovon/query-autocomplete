39fddcc7586f783c7189f8e13d9e6a8aebae7c5f
Salehen Shovon Rahman <sal@linux.com>
2015-05-21 20:13:27 -0700

    Refactored the code.

--- query-autocomplete.js
+++ query-autocomplete.js
@@ -1,56 +1,130 @@
 $(function () {
 
-  window.createTagsInput = createTagsInput;
-  function createTagsInput() {
-    var tags = [];
+  function EventEmitter(){
+    this._callbacks = {}
+  }
 
-    // TODO: soft code the placeholder
-    var tagsInput = $('<div tabindex="-1" class="tags"><input type="text" placeholder="Add tags here..."></div>');
-    var input = tagsInput.find('input[type="text"]');
+  EventEmitter.prototype.on = function (key, callback) {
+    this._callbacks[key] = this._callbacks[key] || [];
+    this._callbacks[key].push(callback);
+  };
 
-    function pushTag(tag) {
-      tags.push(tag);
-      input.val('');
-      tagsInput.find('.tag').remove();
-      tags.slice().reverse().forEach(function (tag) {
-        tagsInput.prepend($('<span class="tag">' + tag + '</span>'));
-      });
-    }
-
-    function popTag() {
-      tags.pop();
-      tagsInput.find('.tag').remove();
-      tags.slice().reverse().forEach(function (tag) {
-        tagsInput.prepend($('<span class="tag">' + tag + '</span>'));
-      });
-    }
-
-    tagsInput.focusin(function () {
-      input.focus();
+  EventEmitter.prototype.emit = function (key) {
+    var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+    this._callbacks[key] && this._callbacks[key].forEach(function (callback) {
+      callback.apply(null, args);
     });
+  };
 
-    input.focus(function (event) {
-      event.stopPropagation();
-    });
+  //////////////////////////////////////////////////////////////////////////////
 
-    function showDropdown(el, list) {
-      var $ul = $(document.createElement);
-      $ul.addClass('dropdown');
-      $ul.css({ top: el})
-    }
+  function QueryPill() {
+    EventEmitter.call(this);
 
-    input.keydown(function (event) {
+    this._value = '';
+  }
+
+  _.extend(QueryPill.prototype, EventEmitter.prototype);
+
+  QueryPill.prototype.render = function () {
+    var self = this;
+
+    this.$el = $('<span></span>');
+
+    // TODO: use a "dropdown input" rather than a regular input.
+    this._$input = $('<input type="text">');
+
+    this._$input.keyDown(function () {
       if (event.keyCode === 13 || event.keyCode === 9) { 
         if (event.keyCode === 9) {
           event.stopPropagation();
           event.preventDefault();
         }
-        pushTag(input.val());
+        self._nextStep();
       } else if (event.keyCode === 8 && input.val() === '') {
-        popTag();
+        throw new Error('Not yet implemented');
       }
     });
-    return tagsInput;
+  };
+
+  QueryPill.prototype._nextStep = function () {
+    throw new Error('Not yet implemented');
+  };
+
+  QueryPill.prototype._updateView = function () {
+    throw new Error('Why does this even exist?');
+  };
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  window.QueryInput = QueryInput;
+  function QueryInput() {
+    var self = this;
+
+    var tags = [];
+    this._tags = tags;
+
+    this.$el = $('<div tabindex="-1" class="tags"></div>');
   }
+
+  QueryInput.prototype.render = function () {
+    var self = this;
+
+    // This is the input that will be detached from the DOM, and reattached.
+    var $input = $(document.createElement('input'));
+
+    $input.attr({
+      type: 'text',
+      // TODO: soft code the placeholder
+      placeholder: 'Add tags here...'
+    });
+    this._$input = $input;
+    this.$el.append($input);
+
+    this.$el.focusin(function () {
+      $input.focus();
+    });
+
+    this._$input.keydown(function (event) {
+      if (event.keyCode === 13 || event.keyCode === 9) { 
+        if (event.keyCode === 9) {
+          event.stopPropagation();
+          event.preventDefault();
+        }
+        self._pushTag($input.val()); // TODO: do something else.
+      } else if (event.keyCode === 8 && $input.val() === '') {
+        self._popTag(); // TODO: do something else.
+      }
+    });
+
+    this._$input.focus(function (event) {
+      event.stopPropagation();
+    });
+
+    return this;
+  };
+
+  QueryInput.prototype._pushTag = function (tag) {
+    var self = this;
+
+    this._tags.push(tag);
+    this._$input.val('');
+
+    // TODO: repetition of code. DRY it out.
+    this.$el.find('.tag').remove();
+    this._tags.slice().reverse().forEach(function (tag) {
+      self.$el.prepend($('<span class="tag">' + tag + '</span>'));
+    });
+  };
+
+  QueryInput.prototype._popTag = function (tag) {
+    var self = this;
+
+    this._tags.pop();
+    this.$el.find('.tag').remove();
+    this._tags.slice().reverse().forEach(function (tag) {
+      self.$el.prepend($('<span class="tag">' + tag + '</span>'));
+    });
+  };
 
 });
